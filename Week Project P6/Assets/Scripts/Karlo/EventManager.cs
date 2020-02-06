8e44ec22633607d2f9bb06cca9947030515f060a
karlo <k.koelewijn2@gmail.com>
2020-02-06 12:43:03 +0100

    finished carrot & grenade apple traps

---
+++ Week Project P6/Assets/Scripts/Karlo/EventManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager
+{
+    public delegate void ResetLevelDelegate();
+
+    public static event ResetLevelDelegate ResetLevelEvent;
+
+    public static void ResetLevelFunc()
+    {
+        ResetLevelEvent();
+    }
+}
--- Week Project P6/Assets/Scripts/Karlo/Trap.cs
+++ Week Project P6/Assets/Scripts/Karlo/Trap.cs
@@ -4,18 +4,38 @@ using UnityEngine;
 
 public class Trap : MonoBehaviour
 {
-    private Player m_player = null;
+    protected Player m_player = null;
+    protected AI m_loli = null;
+
+    protected Coroutine m_invincibleRoutine;
+
+    [SerializeField] protected int m_uses = 3;
 
     private void Start()
     {
         m_player = FindObjectOfType<Player>();
+        m_loli = FindObjectOfType<AI>();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    protected virtual void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject == m_player.gameObject)
+        m_uses--;
+
+        if (m_uses < 1)
         {
-            Debug.Log("Hit Player");
+            Destroy(gameObject);
+        }
+    }
+
+    protected IEnumerator WaitForFlicker()
+    {
+        while (true)
+        {
+            if (m_invincibleRoutine == null)
+            {
+                yield break;
+            }
+            yield return new WaitForSeconds(0);
         }
     }
 }
